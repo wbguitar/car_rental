edaaa35408671665dec2b1786689f1892a557eb3
wbguitar <betti.francesco@gmail.com>
2022-06-08 11:31:21 +0200

    add base solution

---
+++ EFModel/Vehicles/BaseVehicle.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFModel.Vehicles
+{
+    public enum Transmission
+    {
+        Manual,
+        Auto
+    }
+
+    public enum Engines
+    {
+        Petrol,
+        Diesel,
+        Electric
+    }
+
+    public enum Manufacturers
+    {
+        Toyota,
+        Volkswagen,
+        BMW,
+        Mercedes,
+        Ford,
+        Fiat,
+    }
+
+    [Flags]
+    public enum Accessorie
+    {
+        None = 0,
+        AirConditioned = 1,
+        Radio = 2,
+        GPS = 4,
+
+    }
+
+    public abstract class BaseVehicle
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = "";
+        public Manufacturers Manufacturer { get; set; }
+        public Transmission Transmission { get; set; }
+        public Engines Engine { get; set; }
+        public Accessorie Accessories { get; set; } = Accessorie.AirConditioned | Accessorie.Radio;
+    }
+
+}
---
+++ EFModel/Vehicles/Bus.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFModel.Vehicles
+{
+    public class Bus : BaseVehicle
+    {
+        public int ExtraDoors { get; set; } = 0;
+
+    }
+}
