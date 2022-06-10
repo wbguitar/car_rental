0ac88f4ea79acc5492a7676fc2cb968bcfe2680f
wbguitar <betti.francesco@gmail.com>
2022-06-10 17:54:47 +0200

    add Web API service implementation

---
+++ CarRentalService/Controllers/CarRentalController.cs
@@ -0,0 +1,336 @@
+﻿using EFModel.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace CarRentalService.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class VehiclesController : ControllerBase
+    {
+        private readonly ILogger<VehiclesController> _logger;
+
+        public VehiclesController(ILogger<VehiclesController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<dynamic> GetAll()
+        {
+            using (var ctx = new CarRentalContext())
+            {
+                foreach (var v in ctx.Vehicles.ToList())
+                {
+                    yield return v.CreateVehicle(ctx);
+                }
+            }
+        }
+
+        [HttpGet("Id/{id}")]
+        public dynamic GetById(int id)
+        {
+            using (var ctx = new CarRentalContext())
+            {
+                var v = ctx.Vehicles.Where(v => v.Id == id).FirstOrDefault();
+                if (v == null)
+                    return new { errorMessage = $"Vehicle with id={id} doesn't exists" };
+                var obj = v.CreateVehicle(ctx);
+                return obj;
+
+            }
+        }
+
+        [HttpGet("Name/{name}")]
+        public dynamic GetByName(string name)
+        {
+            using (var ctx = new CarRentalContext())
+            {
+                var v = ctx.Vehicles.Where(v => v.Name == name).FirstOrDefault();
+                if (v == null)
+                    return new { errorMessage = $"Vehicle with name={name} doesn't exists" };
+
+                return v.CreateVehicle(ctx);
+
+            }
+        }
+    }
+
+
+    [ApiController]
+    [Route("[controller]")]
+    public class RentController : ControllerBase
+    {
+        private readonly ILogger<RentController> _logger;
+
+        public RentController(ILogger<RentController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("Available/{from}/{to}/{manufacturers}/{transmissions}/{engines}/{categories}/{accessories}/")]
+        public IEnumerable<dynamic> GetAvailables(DateTime from, DateTime to, string manufacturers = "", string transmissions = "",
+            string engines = "", string categories = "", string accessories = "")
+        {
+            using (var ctx = new CarRentalContext())
+            {
+                var mans = manufacturers.Split(' ');
+                var trans = transmissions.Split(' ');
+                var engs = engines.Split(' ');
+                var cats = categories.Split(' ');
+                var accs = accessories.Split(' ');
+                var vehicles = ctx.GetAvailableVehicles(from, to, minStatus: 2, manufacturers: mans, transmissions: trans,
+                    categories: cats, accessories: accs).ToList();
+                foreach (var v in vehicles)
+                {
+                    yield return v.CreateVehicle(ctx);
+                }
+            }
+        }
+
+        [HttpGet("Available/Categories/{categories}/{from}/{to}")]
+        public IEnumerable<dynamic> GetAvailablesByCategories(DateTime from, DateTime to, string categories = "")
+        {
+            using (var ctx = new CarRentalContext())
+            {
+                var cats = categories.Split(' ');
+                var vehicles = ctx.GetAvailableVehicles(from, to, minStatus: 2, categories: cats).ToList();
+                foreach (var v in vehicles)
+                {
+                    yield return v.CreateVehicle(ctx);
+                }
+            }
+        }
+
+        [HttpGet("Available/Manufacturers/{manufacturers}/{from}/{to}")]
+        public IEnumerable<dynamic> GetAvailablesByManufacturer(DateTime from, DateTime to, string manufacturers)
+        {
+            using (var ctx = new CarRentalContext())
+            {
+                var mans = manufacturers.Split(' ');
+                var vehicles = ctx.GetAvailableVehicles(from, to, manufacturers: mans).ToList();
+                foreach (var v in vehicles)
+                {
+                    yield return v.CreateVehicle(ctx);
+                }
+            }
+        }
+
+        [HttpPost("Add/{vehicle}/{customer}/{from}/{to}")]
+        public dynamic PostRent(string vehicle, string customer, DateTime from, DateTime to)
+        {
+            using (var ctx = new CarRentalContext())
+            {
+                try
+                {
+                    var c = ctx.Customers.FirstOrDefault(c => c.Name == customer);
+                    if (c == null)
+                        return StatusCode(StatusCodes.Status500InternalServerError, $"Customer {customer} not found");
+
+                    var vehicles = ctx.GetAvailableVehicles(from, to).ToList();
+                    var v = vehicles.FirstOrDefault(v => v.Name == vehicle);
+                    if (v == null)
+                        return StatusCode(StatusCodes.Status500InternalServerError, $"Vehicle {vehicle} not found");
+
+                    ctx.RentVehicle(customer, vehicle, from, to);
+                    //return CreatedAtAction(nameof(PostRent), new { vehicle = vehicle, from = from, to = to});
+                    return StatusCode(StatusCodes.Status201Created);
+                }
+                catch (Exception e)
+                {
+                    return StatusCode(StatusCodes.Status500InternalServerError, e.Message);
+                }
+            }
+        }
+
+        [HttpDelete("Remove/{vehicle}")]
+        public dynamic DeleteRent(string vehicle)
+        {
+            using (var ctx = new CarRentalContext())
+            {
+                try
+                {
+                    ctx.UnrentVehicle(vehicle);
+                    return StatusCode(StatusCodes.Status202Accepted);
+                }
+                catch (Exception e)
+                {
+                    return StatusCode(StatusCodes.Status500InternalServerError, e.Message);
+                }
+            }
+        }
+    }
+
+    [ApiController]
+    [Route("[controller]")]
+    public class MaintenanceController : ControllerBase
+    {
+        private readonly ILogger<MaintenanceController> _logger;
+
+        public MaintenanceController(ILogger<MaintenanceController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<dynamic> GetUnderMaintenance()
+        {
+            using (var ctx = new CarRentalContext())
+            {
+                return ctx.Maintenances
+                    .Select(m => ctx.Vehicles.First(v => v.Id == m.Vehicle))
+                    .ToList();
+            }
+        }
+
+        [HttpPost]
+        public dynamic PostUnderMaintenance(string vehicleName, DateTime untill)
+        {
+            try
+            {
+                using (var ctx = new CarRentalContext())
+                {
+                    ctx.SendToMaintenance(vehicleName, untill);
+                    var v = ctx.Vehicles.First(v => v.Name == vehicleName);
+
+                    return new
+                    {
+                        status = StatusCode(StatusCodes.Status201Created),
+                        underMaintenance = v.CreateVehicle(ctx)
+                    };
+                }
+            }
+            catch (Exception e)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, e.Message);
+            }
+        }
+
+        [HttpDelete]
+        public dynamic DeleteMaintenance(string vehicleName)
+        {
+            try
+            {
+                using (var ctx = new CarRentalContext())
+                {
+                    ctx.RemoveFromMaintenances(vehicleName);
+                    var v = ctx.Vehicles.FirstOrDefault(v => v.Name == vehicleName);
+                    return new
+                    {
+                        status = StatusCode(StatusCodes.Status202Accepted),
+                        underMaintenance = v.CreateVehicle(ctx)
+                    };
+                }
+            }
+            catch (Exception e)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, e.Message);
+            }
+        }
+    }
+    [ApiController]
+    [Route("[controller]")]
+    public class CustomersController : ControllerBase
+    {
+        private readonly ILogger<CustomersController> _logger;
+
+        public CustomersController(ILogger<CustomersController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<dynamic> GetCustomers()
+        {
+            using (var ctx = new CarRentalContext())
+            {
+                return ctx.Customers.Select(c => new
+                {
+                    name = c.Name,
+                    email = c.Email,
+                    telephone = c.Telephone
+                }).ToList();
+            }
+        }
+
+        [HttpPost]
+        public dynamic PostNewCustomer(string customer, string email = "", string telephone = "")
+        {
+            try
+            {
+                using (var ctx = new CarRentalContext())
+                {
+                    ctx.Customers.Add(new Customer()
+                    {
+                        Name = customer,
+                        Email = email,
+                        Telephone = telephone,
+                    });
+                    ctx.SaveChanges();
+                    return new
+                    {
+                        status = StatusCode(StatusCodes.Status201Created),
+                        customer = ctx.Customers.FirstOrDefault(c => c.Name == customer)
+                    };
+                }
+            }
+            catch (Exception e)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, e.Message);
+            }
+        }
+
+        [HttpDelete]
+        public dynamic DeleteCustomer(string customer)
+        {
+            try
+            {
+                using (var ctx = new CarRentalContext())
+                {
+                    var c = ctx.Customers.FirstOrDefault(c => c.Name == customer);
+                    if (c == null)
+                    {
+                        return StatusCode(StatusCodes.Status204NoContent, $"Customer {customer} not found");
+                    }
+
+                    ctx.Customers.Remove(c);
+                    ctx.SaveChanges();
+                    return new
+                    {
+                        status = StatusCode(StatusCodes.Status202Accepted),
+                        customerDeleted = customer
+                    };
+                }
+            }
+            catch (Exception e)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, e.Message);
+            }
+        }
+
+    }
+    public static class Extensions
+    {
+        public static dynamic CreateVehicle(this Vehicle v, CarRentalContext ctx)
+        {
+            return new
+            {
+                id = v.Id,
+                name = v.Name,
+                manufacturer = v.GetManufacturer(ctx).Name.Trim(),
+                category = v.GetCategory(ctx).Name.Trim(),
+                transmission = v.GetTransmission(ctx).Name.Trim(),
+                engine = v.GetEngine(ctx).Name.Trim(),
+                accessories = v.GetAcessories(ctx)
+                            .Select(a =>
+                            {
+                                return new
+                                {
+                                    name = a.Item2.Name.Trim(),
+                                    status = a.Item1.Status
+                                };
+                            }).ToList()
+            };
+        }
+    }
+}
--- EFModel/Models/Partials.cs
+++ EFModel/Models/Partials.cs
@@ -9,30 +9,57 @@ namespace EFModel.Models
 {
     public partial class CarRentalContext
     {
+        /// <summary>
+        /// Search for a <see cref="Manufacturer"/> from the given manufacturer name
+        /// </summary>
+        /// <param name="mname">Name of the manufacturer to find</param>
+        /// <returns>The manufacturer object or null if not found</returns>
         public Manufacturer? GetManufacturer(string mname)
         {
             return Manufacturers.FirstOrDefault(m => m.Name == mname);
         }
+        /// <summary>
+        /// Search for a <see cref="EngineType"/> from the given engine name
+        /// </summary>
+        /// <param name="etype">Name of the engine to find</param>
+        /// <returns>The manufacturer object or null if not found</returns>
         public EngineType? GetEngineType(string etype)
         {
             return EngineTypes.FirstOrDefault(e => e.Name == etype);
         }
-
+        /// <summary>
+        /// Search for a <see cref="Accessory"/> from the given accessory name
+        /// </summary>
+        /// <param name="aname">Name of the accessory to find</param>
+        /// <returns>The accessory object or null if not found</returns>
         public Accessory? GetAccessory(string aname)
         {
             return Accessories.FirstOrDefault(m => m.Name == aname);
         }
+        /// <summary>
+        /// Search for a <see cref="TransmissionType"/> from the given transmission name
+        /// </summary>
+        /// <param name="tname">Name of the transmission to find</param>
+        /// <returns>The transmission object or null if not found</returns>
         public TransmissionType? GetTransmission(string tname)
         {
             return TransmissionTypes.FirstOrDefault(t => t.Name == tname);
         }
-
+        /// <summary>
+        /// Search for a <see cref="VehicleCategory"/> from the given category name
+        /// </summary>
+        /// <param name="cname">Name of the category to find</param>
+        /// <returns>The category object or null if not found</returns>
         public VehicleCategory? GetVehicleCategory(string cname)
         {
             return VehicleCategories.FirstOrDefault(m => m.Name == cname);
         }
-
-        public IQueryable<Accessory> GetVehicoleAccessories(Vehicle v)
+        /// <summary>
+        /// Gets a list of the <see cref="Accessory"/> from the given vehicle object
+        /// </summary>
+        /// <param name="v">Vehicle object for which to get accessories</param>
+        /// <returns></returns>
+        public IEnumerable<Accessory> GetVehicoleAccessories(Vehicle v)
         {
             return VehicleAccessories
                 .Where(a => a.VehicleId == v.Id)
@@ -102,10 +129,9 @@ namespace EFModel.Models
             var trans = string.Join(',', transmissions ?? new List<string>());
             var accs = string.Join(',', accessories ?? new List<string>());
             var engs = string.Join(',', engines ?? new List<string>());
+            //exec sp_VehicleRequest @dtfrom, @dtto, @minStatus, @accessories, @manufacturers, @engines, @categories, @transmissions
             var query = $"exec sp_VehicleRequest '{from}', '{to}', {minStatus}, '{accs}', '{mans}', '{engs}', '{cats}', '{trans}'";
-            // run query and select vehicle ids
             var vids = Vehicles.FromSqlRaw(query).ToList().Select(v => v.Id);
-            // return actual vehicles objects
             return Vehicles.Where(v => vids.Contains(v.Id));
         }
 
@@ -120,35 +146,13 @@ namespace EFModel.Models
     /// </summary>
     public static class Extensions
     {
+
         /// <summary>
-        /// Converts <see cref="Vehicle"/> object to JSON string
+        /// Gets the <see cref="VehicleCategory"/> for a given vehicle
         /// </summary>
-        /// <param name="v"><see cref="Vehicle"/> object to convert</param>
+        /// <param name="v">Vehicle object for which to get the category</param>
         /// <param name="ctx">Database context</param>
-        /// <returns>JSON string <see cref="Vehicle"/> representation</returns>
-        public static string toJSON(this Vehicle v, CarRentalContext ctx)
-        {
-            var accessories = ctx.VehicleAccessories
-                .Where(a => a.VehicleId == v.Id)
-                .Select(a => $@"
-    {{
-        'Accessory': '{a.Accessory.Name}',
-        'Status': {a.Status}
-    }}").ToList();
-
-            return $@"{{
-'Id': '{v.Id}',
-'Name': '{v.Name}',
-'Category': '{ctx.VehicleCategories.First(c => c.Id == v.Category).Name}',
-'Manufacturer': '{ctx.Manufacturers.First(m => m.Id == v.Manufacturer).Name}',
-'Transmission': '{ctx.TransmissionTypes.First(t => t.Id == v.Transmission).Name}',
-'EngineType': '{ctx.EngineTypes.First(e => e.Id == v.Engine).Name}',
-'Accessories': [
-    {String.Join(",\r\n", accessories)}
-]
-}}";
-        }
-
+        /// <returns>The category of the given vehicle <paramref name="v"/></returns>
         public static VehicleCategory? GetCategory(this Vehicle v, CarRentalContext ctx)
         {
             return ctx.VehicleCategories
@@ -156,32 +160,86 @@ namespace EFModel.Models
                 .FirstOrDefault();
         }
 
+        /// <summary>
+        /// Gets the <see cref="EngineType"/> for a given vehicle
+        /// </summary>
+        /// <param name="v">Vehicle object for which to get the category</param>
+        /// <param name="ctx">Database context</param>
+        /// <returns>The engine of the given vehicle <paramref name="v"/></returns>
         public static EngineType? GetEngine(this Vehicle v, CarRentalContext ctx)
         {
             return ctx.EngineTypes
                 .Where(vc => vc.Id == v.Engine)
                 .FirstOrDefault();
         }
-
+        /// <summary>
+        /// Gets the <see cref="Manufacturer"/> for a given vehicle
+        /// </summary>
+        /// <param name="v">Vehicle object for which to get the category</param>
+        /// <param name="ctx">Database context</param>
+        /// <returns>The manufacturer for the given vehicle <paramref name="v"/></returns>
         public static Manufacturer? GetManufacturer(this Vehicle v, CarRentalContext ctx)
         {
             return ctx.Manufacturers
                 .Where(vc => vc.Id == v.Manufacturer)
                 .FirstOrDefault();
         }
-
+        /// <summary>
+        /// Gets the <see cref="TransmissionType"/> for a given vehicle
+        /// </summary>
+        /// <param name="v">Vehicle object for which to get the category</param>
+        /// <param name="ctx">Database context</param>
+        /// <returns>The transmission of the given vehicle <paramref name="v"/></returns>
         public static TransmissionType? GetTransmission(this Vehicle v, CarRentalContext ctx)
         {
             return ctx.TransmissionTypes
                 .Where(vc => vc.Id == v.Transmission)
                 .FirstOrDefault();
         }
-
-        public static IEnumerable<Accessory>? GetAcessories(this Vehicle v, CarRentalContext ctx)
+        /// <summary>
+        /// Gets the <see cref="VehicleAccessory"/> and related status for a given vehicle
+        /// </summary>
+        /// <param name="v">Vehicle object for which to get the category</param>
+        /// <param name="ctx">Database context</param>
+        /// <returns>A <see cref="Tuple"/> with the <see cref="VehicleAccessory"/> and <see cref="Accessory"/> for the given vehicle <paramref name="v"/></returns>
+        public static IEnumerable<(VehicleAccessory, Accessory)>? GetAcessories(this Vehicle v, CarRentalContext ctx)
         {
-            return ctx.VehicleAccessories
+            var vas = ctx.VehicleAccessories
                 .Where(va => va.VehicleId == v.Id)
-                .Select(va => va.Accessory);
+                .ToList();
+            foreach (var va in vas)
+            {
+                yield return new(va, ctx.Accessories.First((x) => x.Id == va.AccessoryId));
+            }
+        }
+
+        /// <summary>
+        /// Converts <see cref="Vehicle"/> object to JSON string
+        /// </summary>
+        /// <param name="v"><see cref="Vehicle"/> object to convert</param>
+        /// <param name="ctx">Database context</param>
+        /// <returns>JSON string representation of the vehicle <paramref name="v"/></returns>
+        public static string toJSON(this Vehicle v, CarRentalContext ctx)
+        {
+            var accessories = ctx.VehicleAccessories
+                .Where(a => a.VehicleId == v.Id)
+                .Select(a => $@"
+            {{
+                'Accessory': '{a.Accessory.Name}',
+                'Status': {a.Status}
+            }}").ToList();
+
+            return $@"{{
+            'Id': '{v.Id}',
+            'Name': '{v.Name}',
+            'Category': '{ctx.VehicleCategories.First(c => c.Id == v.Category).Name}',
+            'Manufacturer': '{ctx.Manufacturers.First(m => m.Id == v.Manufacturer).Name}',
+            'Transmission': '{ctx.TransmissionTypes.First(t => t.Id == v.Transmission).Name}',
+            'EngineType': '{ctx.EngineTypes.First(e => e.Id == v.Engine).Name}',
+            'Accessories': [
+                {String.Join(",\r\n", accessories)}
+            ]
+            }}";
         }
     }
 }
